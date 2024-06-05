8fb65becf2e8c60cc6dd1d32b02a86cb6d45c939
MuhammadAndra <m.andradzaki@gmail.com>
2024-06-05 12:51:49 +0700

    Overhaul Base

---
+++ resources/js/Shared/Layout.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {Link} from '@inertiajs/vue3'
+</script>
+
+<template>
+    <main>
+        <article>
+            <slot>Welcome to PWL Page!!!</slot>
+        </article>
+        <footer class="flex justify-center pt-6 mt-6 border-t-2 border-teal-500">
+            <div >
+                <Link href="/">Dashboard</Link>
+                <a class="text-teal-500 font-extrabold p-3">|</a>
+                <Link href="/poke">Poke</Link>
+                <a class="text-teal-500 font-extrabold p-3">|</a>
+                <Link href="/user/Nur%20Hazbiy%20Shaffan">Test User</Link>
+            </div>
+        </footer>
+    </main>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,39 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PokeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-Route::get('/', function () {
-    return view('welcome');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::controller(UserController::class)->group(function () {
+    Route::get('/user/{name}', 'index');
+    Route::get('/user', function() {
+        $name = $_GET['name'] ?? 'Default';
+        $userC = new UserController;
+        $userC->index($name);
+    });
+    Route::delete('/user/{name}', 'delete');
 });
+
+Route::controller(PokeController::class)->prefix('/poke')->group(function () {
+    Route::get('/}', 'index');
+    Route::get('/formatted', 'formatted');
+});
+
+Route::get('/poke', [PokeController::class, 'index'])
+    ->name('poke.index');
+
+Route::get('/', [DashboardController::class, 'index'])
+    ->name('dashboard.index');
